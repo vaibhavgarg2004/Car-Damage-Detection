6d07f5f57f3e688134c619ffa63968a4d4f84ce0
Vaibhav Garg <vaibhavgarg152004@gmail.com>
2025-07-01 04:01:37 +0530

    Update model_helper.py

--- model_helper.py
+++ model_helper.py
@@ -43,7 +43,8 @@ def predict(image_path):
     global trained_model
 
     if trained_model is None:
-        trained_model = CarClassifierResNet()               trained_model.load_state_dict( torch.load("model/saved_model.pth",  map_location=torch.device('cpu')))
+        trained_model = CarClassifierResNet()               
+        trained_model.load_state_dict(torch.load("model/saved_model.pth",  map_location=torch.device('cpu')))
         trained_model.eval()
 
     with torch.no_grad():
